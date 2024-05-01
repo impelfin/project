3755ac94b4bd929b718a5799a1eec304cecae093
JunHyen Mun <impelfin@gmail.com>
2024-05-01 14:48:30 +0900

    ㅕㅔ

--- python/data/randomApi/app.py
+++ python/data/randomApi/app.py
@@ -13,9 +13,11 @@ def randomInt(n=None, max=None):
     if (n and max) == None:
         n = 10
         max = 10
-    elif n == None:
+    elif n is None and max is not None:
         n = 10
+        max = int(max)
     elif max == None:
+        n = int(n)
         max = 10
     else:
         n = int(n)
@@ -25,5 +27,5 @@ def randomInt(n=None, max=None):
         while a in alist:
             a = random.randint(1, max) 
         alist.append(a)
-    print({'resultCode' : 200, 'result' : alist})
-    return {'resultCode' : 200, 'result' : alist}
+    print({'resultCode' : 200, 'n' : n, 'max' : max, 'result' : alist})
+    return {'resultCode' : 200, 'n' : n, 'max' : max, 'result' : alist}
