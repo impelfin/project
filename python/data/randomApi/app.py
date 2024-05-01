8d0cf0abd0b05bfe0e058f0594ab805d038ffa9f
JunHyen Mun <impelfin@gmail.com>
2024-05-01 15:10:51 +0900

    up

--- python/data/randomApi/app.py
+++ python/data/randomApi/app.py
@@ -10,18 +10,23 @@ def healthCheck():
 @app.get('/getrandom')
 def randomInt(n=None, max=None):
     alist=[]    
-    if (n and max) == None:
-        n = 10
-        max = 10
-    elif (n is None and max is not None):
-        n = 10
-        max = int(max)
-    elif max == None:
+    if (n is None and max is not None):
+        if int(max) < 10:
+            n = int(max)
+            max = int(max)
+        else:
+            n = 10
+            max = int(max)
+    elif (n is not None and max is None):
         n = int(n)
+        max = 10
+    elif (n and max) is None:
+        n = 10
         max = 10
     else:
         n = int(n)
         max = int(max)
+
     for i in range(n):
         a = random.randint(1, max) 
         while a in alist:
