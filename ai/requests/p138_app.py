b3b3c5754777af1734defa6fd020ae90cf3c156b
impelfin <impelfin@gmail.com>
2024-05-12 08:52:40 +0000

    up

---
+++ ai/requests/p138_app.py
@@ -0,0 +1,14 @@
+import requests  
+
+where_value = 'nexearch' 
+sm_value = 'top_hty'
+fbm_value = 1
+ie_value = 'utf8'
+query_value = 'python'
+
+url = "https://search.naver.com/search.naver"
+parameters = {"where":where_value, "sm":sm_value, "fbm":fbm_value, 
+              "ie":ie_value, "query":query_value}
+r = requests.get(url, params=parameters)
+
+print(r.url)
