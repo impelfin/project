4eaba20312ef3804bc45d853b4a411dac9da654d
impelfin <impelfin@gmail.com>
2024-05-13 04:25:01 +0000

    up

--- ai/openapi/p183_app.py
+++ ai/openapi/p183_app.py
@@ -1,7 +1,7 @@
 import openai
 from IPython.display import Image, display
 
-org_image_file = "C:/myPyAI/data/org_image_for_variation.png" # 원본 이미지
+org_image_file = "./download/org_image_for_variation.png" # 원본 이미지
 
 # 이미지 변형 생성
 response = openai.Image.create_variation( 
@@ -11,8 +11,9 @@ response = openai.Image.create_variation(
 )
     
 image_url = response['data'][0]['url']
+print(image_url)
 
-print("[원본 이미지]")
-display(Image(org_image_file, format='png', width=256, height=256)) # 이미지를 화면에 표시
-print("[생성 이미지]")
-display(Image(image_url, format='png')) # 이미지를 화면에 표시
+# print("[원본 이미지]")
+# display(Image(org_image_file, format='png', width=256, height=256)) # 이미지를 화면에 표시
+# print("[생성 이미지]")
+# display(Image(image_url, format='png')) # 이미지를 화면에 표시
