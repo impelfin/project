4168f92b11070c6fe6f776303022648aa0a3ae88
JunHyeon Mun <impelfin@gmail.com>
2024-07-20 21:46:24 +0900

    up

---
+++ python/yolov8-bs/app.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+model = YOLO('best.pt')
+model.predict(
+   source='https://blog.kakaocdn.net/dn/cakZAB/btrxKhcpgaT/3xK4G4ko3cuDdlO8jxKOn0/img.jpg',
+   conf=0.25,
+   save=True,
+   project="result"
+)
+
