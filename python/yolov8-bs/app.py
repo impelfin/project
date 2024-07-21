943cc7b90711a00c1da7c4497a57b1281311a756
JunHyeon Mun <impelfin@gmail.com>
2024-07-21 13:10:01 +0900

    upup

--- python/yolov8-bs/app.py
+++ python/yolov8-bs/app.py
@@ -1,14 +1,18 @@
 from ultralytics import YOLO
+import torch
 
 # model = YOLO('yolov8n.pt')
-model = YOLO('best.pt')
+model = YOLO('besta.pt')
+
+device = torch.device("mps")
 
 model.predict(
-   source='snac2.png',
+   source='beverage3.png',
    conf=0.25,
    save=True,
    project=".",
    name="result",
-   exist_ok=True
+   exist_ok=True,
+   device=device
 )
 
